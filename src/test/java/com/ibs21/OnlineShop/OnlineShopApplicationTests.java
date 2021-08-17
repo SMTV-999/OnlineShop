54293b0c3338dd1a002a7092227cce8364d6ffcd
Egor Simatov <e.simatov@gmail.com>
2021-08-17 13:02:06 +0400

    Создание проекта РГР

---
+++ src/test/java/com/ibs21/OnlineShop/OnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibs21.OnlineShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
